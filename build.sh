4b1aea32fa1d41e1f0e3a2df1101808399ed31cd
Rhys Arkins <rhys@arkins.net>
2019-05-28 19:27:10 +0200

    Automated build (#32)

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export DATASOURCE=npm
+export LOOKUP_NAME=yarn
+export VERSION_SCHEME=npm
+export START_VERSION=1.0.0
+export IMAGE=yarn
+# export IGNORED_VERSIONS=
+
+npx renovatebot/dockerbuilder
