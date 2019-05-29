1bb7d9a2f154e25d5428187b808b0a37698da93e
Rhys Arkins <rhys@arkins.net>
2019-05-29 08:05:12 +0200

    Use GitHub releases for build

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,5 @@
 FROM renovate/node@sha256:887c567b604505a2a97985f3f7ed4b435e42b0dac09f3220a413038c6878e632
 
-USER root
-
 ARG YARN_VERSION
 
-RUN npm i -g yarn@$YARN_VERSION
-
-RUN chmod -R a+rw /usr/local
-
-RUN chown -R ubuntu:ubuntu /home/ubuntu
-
-USER ubuntu
+RUN curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version $YARN_VERSION
--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-export DATASOURCE=npm
-export LOOKUP_NAME=yarn
-export VERSION_SCHEME=npm
-export START_VERSION=1.0.0
+export DATASOURCE=github
+export LOOKUP_TYPE=releases
+export LOOKUP_NAME=yarnpkg/yarn
+export VERSION_SCHEME=semver
+export START_VERSION=1.5.0
 export IMAGE=yarn
-# export IGNORED_VERSIONS=
+export IGNORED_VERSIONS=1.9.0,1.12.2
 
 npx renovatebot/dockerbuilder
