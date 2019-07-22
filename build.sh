b1789edc68b4889465ff636c61efb374f646bd20
Rhys Arkins <rhys@arkins.net>
2019-07-22 13:20:03 +0200

    use npm datasource

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-export DATASOURCE=github
-export LOOKUP_TYPE=releases
-export LOOKUP_NAME=yarnpkg/yarn
-export VERSION_SCHEME=semver
+export DATASOURCE=npm
+export LOOKUP_NAME=yarn
+export VERSION_SCHEME=npm
 export START_VERSION=1.5.0
 export IMAGE=yarn
 export IGNORED_VERSIONS=1.9.0,1.12.2
