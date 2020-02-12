302fefcfbb4f79cd106e3d02271ae1aa9e0a52da
Rhys Arkins <rhys@arkins.net>
2020-02-12 17:02:03 +0100

    Update excluded versions

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ export LOOKUP_NAME=yarn
 export VERSION_SCHEME=npm
 export START_VERSION=1.5.0
 export IMAGE=yarn
-export IGNORED_VERSIONS=1.9.0,1.12.2
+export IGNORED_VERSIONS=1.9.0,1.12.2,2.0.0-rc.24,2.0.0-rc.27
 
 npx renovatebot/dockerbuilder
